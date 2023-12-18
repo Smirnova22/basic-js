bd2987f99ad7893d2c2024ed43eb5b1f61a2c187
Anastasia Smirnova <shvetsova22@mail.ru>
2023-12-18 16:10:00 +0300

    feat: 27 passing

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -2,7 +2,7 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Implement class DepthCalculator with method calculateDepth
- * that calculates deoth of nested array
+ * that calculates depth of nested array
  * 
  * @example
  * 
@@ -13,9 +13,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    if (!Array.isArray(arr)) {
+      return 0;
+    }
+    
+    if (!arr.some(Array.isArray)) {
+      return 1;
+    }
+    
+    return 1 + Math.max(...arr.map(element => {
+      if (Array.isArray(element)) {
+        return this.calculateDepth(element);
+      } else {
+        return 0;
+      }
+    }));
   }
 }
 
